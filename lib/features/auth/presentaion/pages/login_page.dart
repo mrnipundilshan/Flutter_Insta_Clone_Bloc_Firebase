4c843c0426e66a5a764eb203b76036891cb9692d
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-09-01 08:48:19 +0530

    login page create

---
+++ lib/features/auth/presentaion/pages/login_page.dart
@@ -0,0 +1,18 @@
+/* Login Page */
+
+/*on this page, an exiting user can logi with their, email and password*/
+
+/*once the user successfully logged, they will be rederected to the home page.
+
+if uesr doesnt has account yet, they can go register page to from here to create one */
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

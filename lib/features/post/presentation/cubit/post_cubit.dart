c0a0ebbe05fc726107c927e067cc5237e9f2a93b
mrnipundilshan <nipundilshan991@gmail.com>
2025-09-11 09:19:17 +0530

    fetch all posts

---
+++ lib/features/post/presentation/cubit/post_cubit.dart
@@ -0,0 +1,59 @@
+import 'dart:typed_data';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:insta_clone/features/post/domain/entities/post.dart';
+import 'package:insta_clone/features/post/domain/entities/post_repo.dart';
+import 'package:insta_clone/features/post/presentation/cubit/post_state.dart';
+import 'package:insta_clone/features/storage/domain/storage_repo.dart';
+
+class PostCubit extends Cubit<PostState> {
+  final PostRepo postRepo;
+  final StorageRepo storageRepo;
+
+  PostCubit({required this.postRepo, required this.storageRepo})
+    : super(PostsInitial());
+
+  // create new post
+  Future<void> createPost(
+    Post post, {
+    String? imagePath,
+    Uint8List? imageBytes,
+  }) async {
+    String? imageUrl;
+
+    try {
+      // handle image upload for mobile platforms (using file paht)
+      if (imagePath != null) {
+        emit(PostUploading());
+        imageUrl = await storageRepo.uploadProfileImageMobile(
+          imagePath,
+          post.id,
+        );
+      }
+      // handle image upload for web platforms (using file bytes)
+      else if (imageBytes != null) {
+        emit(PostUploading());
+        imageUrl = await storageRepo.uploadProfileImageWeb(imageBytes, post.id);
+      }
+
+      // give image url to post
+      final newPost = post.copyWith(imageUrl: imageUrl);
+
+      // create post in backend
+      postRepo.createPost(newPost);
+    } catch (e) {
+      throw Exception("failed to create post: $e");
+    }
+  }
+
+  // fetch all posts
+  Future<void> fetchAllPosts() async {
+    try {
+      emit(PostsLoading());
+      final posts = await postRepo.fetchAllPosts();
+      emit(PostsLoaded(posts));
+    } catch (e) {
+      emit(PostsError("Failed to fetch posts: $e"));
+    }
+  }
+}

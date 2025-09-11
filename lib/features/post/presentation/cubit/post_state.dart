ae7730cafe2b2ecd67c0bdba3ae6ac969ee9f29f
mrnipundilshan <nipundilshan991@gmail.com>
2025-09-11 07:25:59 +0530

    post states

---
+++ lib/features/post/presentation/cubit/post_state.dart
@@ -0,0 +1,30 @@
+/*
+
+Post States
+
+*/
+
+import 'package:insta_clone/features/post/domain/entities/post.dart';
+
+abstract class PostState {}
+
+// initial
+class PostsInitial extends PostState {}
+
+// loading..
+class PostsLoading extends PostState {}
+
+// uploading..
+class PostUploading extends PostState {}
+
+// error
+class PostsError extends PostState {
+  final String message;
+  PostsError(this.message);
+}
+
+//loaded
+class PostsLoaded extends PostState {
+  final List<Post> Posts;
+  PostsLoaded(this.Posts);
+}

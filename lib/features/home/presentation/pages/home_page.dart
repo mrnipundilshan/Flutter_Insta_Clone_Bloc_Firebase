6ed6c8050970c9ee50762cd4c4f9f3c9e7b31157
mrnipundilshan <nipundilshan991@gmail.com>
2025-09-11 19:12:56 +0530

    post fetch

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:insta_clone/features/home/presentation/components/my_drawer.dart';
+import 'package:insta_clone/features/post/presentation/cubit/post_cubit.dart';
+import 'package:insta_clone/features/post/presentation/cubit/post_state.dart';
 import 'package:insta_clone/features/post/presentation/pages/upload_post_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +14,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // post cubit
+  late final postCubit = context.read<PostCubit>();
+
+  // on startup
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch all posts
+    fetchAllPosts();
+  }
+
+  void fetchAllPosts() {
+    postCubit.fetchAllPosts();
+  }
+
+  void deletePost(String postId) {
+    postCubit.deletePost(postId);
+    fetchAllPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +56,47 @@ class _HomePageState extends State<HomePage> {
 
       // drawer
       drawer: MyDrawer(),
+
+      // body
+      body: BlocBuilder<PostCubit, PostState>(
+        builder: (context, state) {
+          // loading
+          if (state is PostsLoading && state is PostUploading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          // loaded
+          else if (state is PostsLoaded) {
+            final allPosts = state.posts;
+
+            if (allPosts.isEmpty) {
+              return const Center(child: Text("No Posts available"));
+            }
+            return ListView.builder(
+              itemCount: allPosts.length,
+              itemBuilder: (context, index) {
+                // get individual post
+                final post = allPosts[index];
+
+                // image
+                return CachedNetworkImage(
+                  imageUrl: post.imageUrl,
+                  height: 430,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const SizedBox(height: 430),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                );
+              },
+            );
+          }
+          // error
+          else if (state is PostsError) {
+            return Center(child: Text(state.message));
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
     );
   }
 }
